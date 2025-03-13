09ecc170efd4fc5cd192e6ec06b81905b92299f8
Michael Bo Poulsen <mbp@solita.dk>
2025-03-13 13:43:59 +0100

    New game! i18n Day 1

---
+++ i18n/2025/1/Day1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+List<string> inputLines = [.. File.ReadAllLines("input.txt")];
+
+int byteCountLimit = 160; // SMS
+int charCountLimit = 140; // Tweet
+
+List<int> cents = [];
+
+foreach (var input in inputLines)
+{
+    // Byte count
+    int byteCount = Encoding.UTF8.GetByteCount(input);
+
+    // Character count
+    int charCount = input.Length;
+
+    int cent = 0;
+
+    if (byteCount <= byteCountLimit && charCount <= charCountLimit)
+    {
+        // Send for both SMS and Tweet
+        cent = 13;
+    }
+    else if (byteCount <= byteCountLimit)
+    {
+        // Send as SMS
+        cent = 11;
+    }
+    else if (charCount <= charCountLimit)
+    {
+        // Send as Tweet
+        cent = 7;
+    }
+
+    cents.Add(cent);
+}
+
+// Calculate total cost
+int totalCost = cents.Sum();
+
+// Show formula
+var output = "";
+foreach (var cent in cents)
+{
+    output += cent + " + ";
+}
+
+output = output[..^3];
+output += " = " + totalCost;
+
+Console.WriteLine("Formula: " + output);
+Console.ReadLine();
