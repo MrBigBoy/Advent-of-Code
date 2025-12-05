7fd90d9bad28d254e93b8fe7a2053dead2f00687
Michael Bo Poulsen <mbp@solita.dk>
2025-12-05 13:06:31 +0100

    Adds solution for Day 4 challenges
    Implements solutions for both parts of the Day 4 challenge, which involves optimizing forklift paper roll removal based on adjacency rules.
    
    Part 1 calculates the number of accessible paper rolls.
    Part 2 extends this logic to iteratively remove accessible rolls until no more can be removed, providing a total count.

---
+++ 2025/Day4-1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Day4_1;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var runAsTest = false;
+        var path = runAsTest
+            ? Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "testinput.txt"))
+            : Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "input.txt"));
+
+        var lines = File.ReadAllLines(path).Where(l => l.Length > 0).ToArray();
+        var height = lines.Length;
+        var width = height > 0 ? lines[0].Length : 0;
+
+        var total = 0;
+        for (var r = 0; r < height; r++)
+        {
+            var row = lines[r];
+            for (var c = 0; c < width; c++)
+            {
+                if (row[c] != '@')
+                    continue;
+
+                var adjacentRolls = 0;
+                for (var dr = -1; dr <= 1; dr++)
+                {
+                    for (var dc = -1; dc <= 1; dc++)
+                    {
+                        if (dr == 0 && dc == 0)
+                            continue;
+                        var nr = r + dr;
+                        var nc = c + dc;
+                        if (nr < 0 || nr >= height || nc < 0 || nc >= width)
+                            continue;
+                        if (lines[nr][nc] == '@')
+                            adjacentRolls++;
+                    }
+                }
+
+                if (adjacentRolls < 4)
+                    total++;
+            }
+        }
+
+        Console.WriteLine("Rolls of paper that can be accessed by the forklift: {0}", total);
+    }
+}
---
+++ 2025/Day4-2/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Day4_2;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var runAsTest = false;
+        var path = runAsTest
+            ? Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "testinput.txt"))
+            : Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "input.txt"));
+
+        var lines = File.ReadAllLines(path).Where(l => l.Length > 0).ToArray();
+        var height = lines.Length;
+        var width = height > 0 ? lines[0].Length : 0;
+
+        // Use a mutable grid
+        var grid = new char[height][];
+        for (var r = 0; r < height; r++)
+            grid[r] = lines[r].ToCharArray();
+
+        var totalRemoved = 0;
+        while (true)
+        {
+            var toRemove = new List<(int r, int c)>();
+            for (var r = 0; r < height; r++)
+            {
+                for (var c = 0; c < width; c++)
+                {
+                    if (grid[r][c] != '@')
+                        continue;
+
+                    var adjacentRolls = 0;
+                    for (var dr = -1; dr <= 1; dr++)
+                    {
+                        for (var dc = -1; dc <= 1; dc++)
+                        {
+                            if (dr == 0 && dc == 0)
+                                continue;
+                            var nr = r + dr;
+                            var nc = c + dc;
+                            if (nr < 0 || nr >= height || nc < 0 || nc >= width)
+                                continue;
+                            if (grid[nr][nc] == '@')
+                                adjacentRolls++;
+                        }
+                    }
+
+                    if (adjacentRolls < 4)
+                        toRemove.Add((r, c));
+                }
+            }
+
+            if (toRemove.Count == 0)
+                break;
+
+            foreach (var (r, c) in toRemove)
+                grid[r][c] = '.'; // remove
+
+            totalRemoved += toRemove.Count;
+        }
+
+        Console.WriteLine("Total rolls of paper that can be removed: {0}", totalRemoved);
+    }
+}
